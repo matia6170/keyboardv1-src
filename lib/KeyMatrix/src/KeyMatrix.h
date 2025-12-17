7dcb82b5963584ed8ff5de1fd4aa9d1a3ce44705
matia6170 <matia6170@gmail.com>
2025-12-17 13:10:16 -0600

    now using the tinyUSB stack, and implemented composite USB with hidkeyboard,msc,serialport. while USB HID keyboard worked with the hardware mode, it cant do compsite USB.

--- lib/KeyMatrix/src/KeyMatrix.h
+++ lib/KeyMatrix/src/KeyMatrix.h
@@ -14,15 +14,19 @@ class KeyMatrix {
     int _rowPins[ROWS];
     int _colPins[COLS];
     bool _lastKeyState[ROWS][COLS];
-    // bool _lastKeyState[ROWS][COLS];
+    char _keyMap[ROWS][COLS];
 
-    void (*_key_evt_callback)(int row, int col, bool down);
+    void (*_key_evt_callback)(int row, int col, bool down, char key) = nullptr;
+
+    const int DEBOUNCE_DURATION_MS = 5;
+    long lastScanTime = 0;
 
    public:
     KeyMatrix(const int (&rowPins)[ROWS], const int (&colPins)[COLS]);
     void init();
+    void init_key_map(char (&keymap)[ROWS][COLS]);
     void scan_keys();
-    void register_callback(void (*key_evt_callback)(int row, int col, bool down));
+    void register_callback(void (*key_evt_callback)(int row, int col, bool down, char key));
 };
 
 template <size_t ROWS, size_t COLS>
@@ -49,44 +53,56 @@ void KeyMatrix<ROWS, COLS>::init() {
     for (int i = 0; i < ROWS; i++) {
         pinMode(_rowPins[i], INPUT);
     }
+    for (int r = 0; r < ROWS; r++)
+        for (int c = 0; c < COLS; c++) _keyMap[r][c] = 0x00;
+
+}
+
+template <size_t ROWS, size_t COLS>
+void KeyMatrix<ROWS, COLS>::init_key_map(char (&keymap)[ROWS][COLS]) {
+    for (int r = 0; r < ROWS; r++)
+        for (int c = 0; c < COLS; c++) _keyMap[r][c] = keymap[r][c];
 }
 
 template <size_t ROWS, size_t COLS>
 void KeyMatrix<ROWS, COLS>::scan_keys() {
-    // Iterate through each column (Driver)
-    for (int c = 0; c < COLS; c++) {
-        // select current col
-        digitalWrite(_colPins[c], HIGH);
+    if (millis() - lastScanTime > DEBOUNCE_DURATION_MS) {
+        // Iterate through each column (Driver)
+        for (int c = 0; c < COLS; c++) {
+            // select current col
+            digitalWrite(_colPins[c], HIGH);
 
-        // Iterate through each row (Sensor)
-        for (int r = 0; r < ROWS; r++) {
-            bool currentKey = digitalRead(_rowPins[r]);
+            // Iterate through each row (Sensor)
+            for (int r = 0; r < ROWS; r++) {
+                bool currentKey = digitalRead(_rowPins[r]);
 
-            // Key down
-            if (currentKey == HIGH && _lastKeyState[r][c] == false) {
-                // Serial.print("Key DOWN: ");
-                // Serial.println(keyMap[r][c]);
-                _key_evt_callback(r,c,true);
-                // Serial.printf("(%d, %d) DOWN\n", c, r);
-                // Update the state
-                _lastKeyState[r][c] = true;
+                // Key down
+                if (currentKey == HIGH && _lastKeyState[r][c] == false) {
+                    // Serial.print("Key DOWN: ");
+                    // Serial.println(keyMap[r][c]);
+                    if (_key_evt_callback != nullptr) _key_evt_callback(r, c, true, _keyMap[r][c]);
+                    // Serial.printf("(%d, %d) DOWN\n", c, r);
+                    // Update the state
+                    _lastKeyState[r][c] = true;
+                }
+                // Key up
+                else if (currentKey == LOW && _lastKeyState[r][c] == true) {
+                    if (_key_evt_callback != nullptr) _key_evt_callback(r, c, false, _keyMap[r][c]);
+                    // Serial.printf("(%d, %d) UP\n", c, r);
+                    // Update the state
+                    _lastKeyState[r][c] = false;
+                }
             }
-            // Key up
-            else if (currentKey == LOW && _lastKeyState[r][c] == true) {
-                _key_evt_callback(r,c,false);
-                // Serial.printf("(%d, %d) UP\n", c, r);
-                // Update the state
-                _lastKeyState[r][c] = false;
-            }
+
+            // set current col to 0
+            digitalWrite(_colPins[c], LOW);
         }
-
-        // set current col to 0
-        digitalWrite(_colPins[c], LOW);
+        lastScanTime = millis();
     }
 }
 
 template <size_t ROWS, size_t COLS>
-void KeyMatrix<ROWS, COLS>::register_callback(void (*key_evt_callback)(int row, int col, bool down)) {
+void KeyMatrix<ROWS, COLS>::register_callback(void (*key_evt_callback)(int row, int col, bool down, char key)) {
     _key_evt_callback = key_evt_callback;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <Arduino.h>
-
+#include "USB.h"
+#include "USBHIDKeyboard.h"
 #include "KeyMatrix.h"
+#include "M5Unified.h"
+
+#include "USBMSC.h"
+
 
 #define ROWS 4
 #define COLS 3
@@ -8,15 +13,72 @@
 const int rowPins[ROWS] = {3, 4, 6, 40};
 const int colPins[COLS] = {5, 39, 14};
 
-KeyMatrix<ROWS, COLS> Keypad(rowPins, colPins);
+char keyMap[ROWS][COLS] = {
+  {'1', '2', '3'},
+  {'4', '5', '6'},
+  {'7', '8', '9'},
+  {' ', '0', '1'}
+};
 
-void key_evt_callback(int row, int col, bool down) { Serial.printf("Callback: (%d, %d) %s\n", col, row, down ? "DOWN" : "UP"); }
+KeyMatrix<ROWS, COLS> Keypad(rowPins, colPins);
+USBHIDKeyboard Keyboard;
+USBMSC msc;
+
+
+bool blue;
+void key_evt_callback(int row, int col, bool down, char key) { 
+  Serial.printf("Callback: %c (%d, %d) %s\n", key, col, row, down ? "DOWN" : "UP");
+  if (down)
+    Keyboard.press(key);
+  else
+    Keyboard.release(key);
+
+  blue = down;
+  
+}
+
 
 void setup() {
     // put your setup code here, to run once:
-    Serial.begin(115200);
-    Keypad.register_callback(key_evt_callback);
-    Keypad.init();
-}
+    // 1. Mandatory for Mode 0 Keyboard
+msc.vendorID("M5Stack");
+  msc.productID("StampS3-Disk");
 
-void loop() { Keypad.scan_keys(); }
+  msc.begin(512, 100); // 512 bytes per block, 100 blocks total (example)
+    
+  
+  blue = false;
+    Keyboard.begin();
+    Serial.begin();
+    
+    USB.begin();//this line needs to be called at the end.
+
+    // USBSerial.begin(115200);
+    
+    Keypad.init();
+    Keypad.register_callback(key_evt_callback);
+    Keypad.init_key_map(keyMap);
+
+    auto cfg = M5.config();
+    M5.begin(cfg);
+    int textsize = M5.Display.height() / 60;
+    if (textsize == 0) {
+        textsize = 1;
+    }
+    M5.Display.setTextSize(textsize);
+    // Set cursor position (x, y)
+    M5.Display.setCursor(10, 10);
+
+    // Print text
+    M5.Display.print("ARDUINO_USB_MODE: ");
+    M5.Display.println(ARDUINO_USB_MODE);
+
+
+  }
+
+void loop() { 
+  Keypad.scan_keys(); 
+  M5.Display.fillRect(70, 50, 50, 50, blue?BLUE:RED);
+  M5.update();
+
+}
