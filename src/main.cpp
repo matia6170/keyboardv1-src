b3ac1eb754ad498a18b0edc159a33b1628908d89
matia6170 <matia6170@gmail.com>
2026-05-18 23:35:37 -0700

    before edit

--- src/main.cpp
+++ src/main.cpp
@@ -26,12 +26,16 @@ USBMSC msc;
 
 
 bool blue;
+char curr_key;
 void key_evt_callback(int row, int col, bool down, char key) { 
   Serial.printf("Callback: %c (%d, %d) %s\n", key, col, row, down ? "DOWN" : "UP");
-  if (down)
+  if (down){
     Keyboard.press(key);
-  else
+    curr_key = key;
+  }
+  else{
     Keyboard.release(key);
+  }
 
   blue = down;
   
@@ -41,17 +45,24 @@ void key_evt_callback(int row, int col, bool down, char key) {
 void setup() {
     // put your setup code here, to run once:
     // 1. Mandatory for Mode 0 Keyboard
-msc.vendorID("M5Stack");
-  msc.productID("StampS3-Disk");
 
+    // disc 
+  msc.vendorID("M5Stack");
+  msc.productID("StampS3-Disk");
   msc.begin(512, 100); // 512 bytes per block, 100 blocks total (example)
     
   
   blue = false;
+  curr_key = 0;
+  // inits hid keyboard
     Keyboard.begin();
+
+    // gets translated in to usbserial when using tiny usb
     Serial.begin();
     
-    USB.begin();//this line needs to be called at the end.
+  //this line needs to be called at the end. 
+  // Initializes (upto 6) devices
+    USB.begin();
 
     // USBSerial.begin(115200);
     
@@ -67,18 +78,23 @@ msc.vendorID("M5Stack");
     }
     M5.Display.setTextSize(textsize);
     // Set cursor position (x, y)
-    M5.Display.setCursor(10, 10);
+    M5.Display.setCursor(10, 0);
 
     // Print text
     M5.Display.print("ARDUINO_USB_MODE: ");
     M5.Display.println(ARDUINO_USB_MODE);
+    M5.Display.println(textsize);
 
 
   }
 
 void loop() { 
   Keypad.scan_keys(); 
+  M5.Display.setCursor(10, 20);
+  M5.Display.print(curr_key);
+  
   M5.Display.fillRect(70, 50, 50, 50, blue?BLUE:RED);
+
   M5.update();
 
 }
